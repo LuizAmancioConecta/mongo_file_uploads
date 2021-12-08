9c9206353bc7ff59a0bdbed7a70e8864c18817a1
Luiz Eduardo Gagno Amancio <luiz.amancio@conecta.tech>
2021-12-08 11:44:07 -0300

    .

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
 // Mongo URI
-const mongoURI = 'mongodb://brad:brad@ds257838.mlab.com:57838/mongouploads';
+const mongoURI = 'mongodb://localhost/conecta-supply-db';
 
 // Create mongo connection
 const conn = mongoose.createConnection(mongoURI);
